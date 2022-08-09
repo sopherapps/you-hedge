a689b132475fa9be8c333531c5f6a28dec01b35a
Martin <sales@sopherapps.com>
2022-08-09 15:14:36 +0300

    Add back button on player

--- src/pages/player.page.tsx
+++ src/pages/player.page.tsx
@@ -1,4 +1,4 @@
-import { useContext, useLayoutEffect } from "react";
+import { useCallback, useContext, useLayoutEffect } from "react";
 import { BrowserHistory } from "history";
 import { UNSAFE_NavigationContext, useNavigate, useParams, useSearchParams } from "react-router-dom"
 
@@ -6,8 +6,12 @@ export default function PlayerPage() {
     const { videoId } = useParams();
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
-
     const navigation = useContext(UNSAFE_NavigationContext).navigator as BrowserHistory;
+
+    const goBack = useCallback(() => {
+        navigate(-1);
+    }, [navigate]);
+
     useLayoutEffect(() => {
         if (navigation) {
             navigation.listen(({ action, location }) => {
@@ -21,6 +25,10 @@ export default function PlayerPage() {
 
 
     return <div className="player h-100vh w-100vw">
+        <div className="floating-btn text-center" onClick={goBack}>
+            <div className="text-center icon i-back">&larr;</div>
+            <div className="text-center">Back</div>
+        </div>
         <iframe
             title={searchParams.get("title") || "A Youtube Video"}
             src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
